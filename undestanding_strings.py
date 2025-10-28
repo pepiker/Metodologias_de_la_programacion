27047bebce42fa8bc2ede6dd1a9045bbde59d3f2
pepiker <2530077@upv.edu.mx>
2025-10-28 10:44:46 -0600

    reposiker

---
+++ undestanding_strings.py
@@ -0,0 +1,15 @@
+"""
+
+    Un string es una manera sencilla una serie 
+    de caracteres. En python , todo lo que se encuentre 
+    dentro de las comillas (' ') o dobles (" ")
+    sera considerado un string.
+
+    Ejemplo:
+    "Esto es un string"
+    'Esto es un string'
+
+    'Le dije a un amigo "python es mi lenguaje favorito"'
+    "El lenguaje 'python' lleva el nombre por Monty Python , no por ser la serpiente"
+
+"""
